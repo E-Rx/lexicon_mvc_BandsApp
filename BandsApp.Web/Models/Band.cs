2236f3e2807809ffc1d38df9e5e97a0891871ecb
Zakk Elfving <zakk.elfving@gmail.com>
2025-05-13 09:29:50 +0200

    removed zakksrating

--- BandsApp.Web/Models/Band.cs
+++ BandsApp.Web/Models/Band.cs
@@ -4,8 +4,6 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; } 
-        public string? Description { get; set; } = null;
-
-        public int? zakksrating;
+        public string? Description { get; set; } = null!;
     }
 }
