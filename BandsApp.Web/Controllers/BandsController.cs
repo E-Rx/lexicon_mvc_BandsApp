66aef04697c0627097b69f96c69704bdc3f19bda
elise rochaix <elise.rochaix@gmail.com>
2025-05-13 16:53:08 +0200

    original group project

--- BandsApp.Web/Controllers/BandsController.cs
+++ BandsApp.Web/Controllers/BandsController.cs
@@ -7,20 +7,20 @@ namespace BandsApp.Web.Controllers;
 
 public class BandsController : Controller
 {
-    BandService bandService = new BandService(); 
+  BandService bandService = new BandService();
 
-    [Route("")]
-    public IActionResult Index()
-    {
-        var bands = bandService.GetAllBands();
-        return View(bands);
-    }
-    [Route ("details/{id}")]
+  [Route("")]
+  public IActionResult Index()
+  {
+    var bands = bandService.GetAllBands();
+    return View(bands);
+  }
+  [Route("details/{id}")]
 
-    public IActionResult Details(int id)
-    {
-        var band = bandService.GetById(id);
-        return View(band);
-    }
+  public IActionResult Details(int id)
+  {
+    var band = bandService.GetById(id);
+    return View(band);
+  }
 
 }
--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -1,43 +1,43 @@
 ﻿using BandsApp.Web.Models;
 namespace BandsApp.Web.Services
 {
-    public class BandService
-    {
-        List<Band> bands = [
+  public class BandService
+  {
+    List<Band> bands = [
 
-           new Band {
-                   Id = 1,
-                   Name = "The Baboon Show",
-                   Description ="The Baboon Show är ett svenskt punkband som började som ett renodlat liveband 2003.",
-                   ImagePath = "/Images/BaboonShow.jpg",
-                   VideoUrl = "https://www.youtube.com/embed/pcx-Qzjmm5A?si=npfoqrg3-ItpcX_I",
-                   Albums = ["This is it!", "Boredom Boredom Go Away!", "It's A Sin", "Don't Don't Don't", "Pep Talk", "Betsy's Revenge",
-                    "Money Money Money", "Punk Rock Harbour", "People's Republic Of The Baboon Show Formerly Known As Sweden", "Damnation", "Havanna Sessions",
-                    "The World Is Bigger Than You", "Best of 2.0", "Radio Rebelde", "I never say goodnight", "Odball", "God Bless You All"]},
-           new Band {
-               Id = 2,
-               Name = "Therapie TAXI",
-               Description ="Thérapie TAXI var ett franskt pop-rock/électro-band aktivt mellan 2016 och 2021, känt för sina provokativa texter och energi.",
-               ImagePath = "/Images/therapieTaxi.jpeg",
-               VideoUrl="https://www.youtube.com/embed/CpTLFchI8Zk?si=fUiEf-Lk13u6zlqI",
-            Albums = ["Hit Sale", "Cadavre exquis"]},
-           new Band {
-               Id = 3,
-               Name = "Alanis Morissette",
-               Description = "A Canadian musician known for her confessional lyrics and a layered rock-influenced sound.",
-               ImagePath = "/Images/AlanisMorissette.jpg",
-               VideoUrl = "https://www.youtube.com/embed/Jne9t8sHpUc?si=mUFKVAytIB8yT0Z0",
-               Albums = ["Alanis", "Now Is The Time", "Jagged Little Pill", "Supposed Former Infatuation Junkie", "Under Rug Swept", "So Called Chaos", "Flavors Of Entaglement", "Havoc And Bright Lights", "Such Pretty Forks in the Road"]
-           }
-           ];
-     
+       new Band {
+          Id = 1,
+          Name = "The Baboon Show",
+          Description ="The Baboon Show är ett svenskt punkband som började som ett renodlat liveband 2003.",
+          ImagePath = "/Images/BaboonShow.jpg",
+          VideoUrl = "https://www.youtube.com/embed/pcx-Qzjmm5A?si=npfoqrg3-ItpcX_I",
+          Albums = ["This is it!", "Boredom Boredom Go Away!", "It's A Sin", "Don't Don't Don't", "Pep Talk", "Betsy's Revenge",
+          "Money Money Money", "Punk Rock Harbour", "People's Republic Of The Baboon Show Formerly Known As Sweden", "Damnation", "Havanna Sessions",
+          "The World Is Bigger Than You", "Best of 2.0", "Radio Rebelde", "I never say goodnight", "Odball", "God Bless You All"]},
+      new Band {
+          Id = 2,
+          Name = "Therapie TAXI",
+          Description ="Thérapie TAXI var ett franskt pop-rock/électro-band aktivt mellan 2016 och 2021, känt för sina provokativa texter och energi.",
+          ImagePath = "/Images/therapieTaxi.jpeg",
+          VideoUrl="https://www.youtube.com/embed/CpTLFchI8Zk?si=fUiEf-Lk13u6zlqI",
+          Albums = ["Hit Sale", "Cadavre exquis"]},
+      new Band {
+          Id = 3,
+          Name = "Alanis Morissette",
+          Description = "A Canadian musician known for her confessional lyrics and a layered rock-influenced sound.",
+          ImagePath = "/Images/AlanisMorissette.jpg",
+          VideoUrl = "https://www.youtube.com/embed/Jne9t8sHpUc?si=mUFKVAytIB8yT0Z0",
+          Albums = ["Alanis", "Now Is The Time", "Jagged Little Pill", "Supposed Former Infatuation Junkie", "Under Rug Swept", "So Called Chaos", "Flavors Of Entaglement", "Havoc And Bright Lights", "Such Pretty Forks in the Road"]
+        }
+       ];
 
-        public Band? GetById(int id) => bands
-            .SingleOrDefault(b => b.Id == id);
 
-        public Band[] GetAllBands() => bands
-            .OrderBy(o => o.Name)
-            .ToArray();
+    public Band? GetById(int id) => bands
+        .SingleOrDefault(b => b.Id == id);
 
-    }
+    public Band[] GetAllBands() => bands
+        .OrderBy(o => o.Name)
+        .ToArray();
+
+  }
 }
