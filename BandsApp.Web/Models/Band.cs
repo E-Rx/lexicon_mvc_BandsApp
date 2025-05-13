72da3a47c2c63561bea5d1d2d827231930192200
Nowaxial <Nowaxial@users.noreply.github.com>
2025-05-13 09:29:30 +0200

    Changed to null! in Band Model

--- BandsApp.Web/Models/Band.cs
+++ BandsApp.Web/Models/Band.cs
@@ -3,8 +3,8 @@
     public class Band
     {
         public int Id { get; set; }
-        public string? Name { get; set; } 
-        public string? Description { get; set; } = null;
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
 
         public int? zakksrating;
     }
