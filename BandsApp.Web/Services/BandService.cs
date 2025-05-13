977b25b435b861b1742f09008c9178e398b47cc8
Nowaxial <Nowaxial@users.noreply.github.com>
2025-05-13 10:14:51 +0200

    Added Band List

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -4,9 +4,11 @@ namespace BandsApp.Web.Services
     public class BandService
     {
         List<Band> bands = [
-            
-            
-            
+
+           new Band { Id = 1, Name = "The Baboon Show", Description ="The Baboon Show är ett svenskt punkband som började som ett renodlat liveband 2003."},
+           new Band { Id = 2, Name = "Therapie TAXI", Description ="Thérapie TAXI var ett franskt pop-rock/électro-band aktivt mellan 2016 och 2021, känt för sina provokativa texter och energi."},
+           new Band { Id = 3, Name = "Alanis Morrissette", Description ="A Canadian musician known for her confessional lyrics and a layered rock-influenced sound."}
          ];
+
     }
 }
